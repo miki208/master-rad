aae259c7b9a9bb2bc5fa92e2f8c463c8be1adeae
Milos Samardzija <miloss208@gmail.com>
2021-06-27 10:21:10 +0200

    Minor fixes in APIGateway::UserController

--- MyRunningBuddy/APIGateway/app/Http/Controllers/UserController.php
+++ MyRunningBuddy/APIGateway/app/Http/Controllers/UserController.php
@@ -58,6 +58,6 @@ class UserController extends Controller
         if($response == null)
             return ResponseHelper::GenerateInternalServiceUnavailableErrorResponse();
 
-        return response()->json($response, Response::HTTP_OK, [], JSON_UNESCAPED_SLASHES);
+        return response()->json($response->json(), Response::HTTP_OK, [], JSON_UNESCAPED_SLASHES);
     }
 }
